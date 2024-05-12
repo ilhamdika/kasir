ac13e9f04a0fd1950f369272bf27af8f452e86ac
ilhamdika <ilham27dika@gmail.com>
2024-05-12 12:41:51 +0700

    sidebar new ui

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -1,7 +1,22 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="/" class="brand-link">
+<style>
+    .logo{
+        text-decoration: none;
+    }
+
+    .logo:hover{
+        color: #000000;
+    }
+    .text_logo {
+        font-size: 20px;
+        font-weight: bold;
+        text-decoration: none;
+    }
+</style>
+
+<aside class="main-sidebar sidebar-primary elevation-4">
+    <a href="/" class="brand-link mt-3 mb-3 logo">
         <img src="/images/logo2.jpg" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+        <span class="text_logo">Kasirku 💸</span>
     </a>
     <div class="sidebar">
         <nav class="mt-2">
