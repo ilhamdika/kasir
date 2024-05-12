1d02d7eee42a84e6f2bb3d88037fa62db15bd6c3
ilhamdika <ilham27dika@gmail.com>
2024-05-12 13:16:16 +0700

    new col-row add

--- resources/views/menu/create.blade.php
+++ resources/views/menu/create.blade.php
@@ -5,7 +5,7 @@
  'icon'=>'fas fa-utensils'
 ]">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-12">
             <form class="card card-primary" action="{{ route('menu.store') }}" method="POST"
                 enctype="multipart/form-data">
                 <div class="card-header">
@@ -13,30 +13,39 @@
                 </div>
                 <div class="card-body">
                     @csrf
-                    <x-group>
-                        <label>Nama Menu</label>
-                        <x-input name="nama_menu" />
-                    </x-group>
-                    <x-group>
-                        <label>Harga</label>
-                        <x-input name="harga" type="number" />
-                    </x-group>
-                    <x-group>
-                        <label>Stok</label>
-                        <x-input name="stok" type="number" />
-                    </x-group>
-                    <x-group>
-                        <label>File Foto/Gambar</label>
-                        <x-input name="file_foto" type="file" />
-                    </x-group>
-                    <x-group>
-                        <label>Kategori</label>
-                        <x-select name="kategori" :data-option="[
-                            ['value'=>'','option'=>'Pilih'],
-                            ['value'=>'makanan','option'=>'Makanan'],
-                            ['value'=>'minuman','option'=>'Minuman'],
-                            ]" />
-                    </x-group>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <x-group>
+                                <label>Nama Menu</label>
+                                <x-input name="nama_menu" />
+                            </x-group>
+                            <x-group>
+                                <label>Harga</label>
+                                <x-input name="harga" type="number" />
+                            </x-group>
+                            <x-group>
+                                <label>Stok</label>
+                                <x-input name="stok" type="number" />
+                            </x-group>
+                        </div>
+                        <div class="col-md-6">
+                            <x-group>
+                                <label>File Foto/Gambar</label>
+                                <x-input name="file_foto" type="file" />
+                            </x-group>
+                            <x-group>
+                                <label>Kategori</label>
+                                <select name="kategori" class="form-control">
+                                    <option value="">Pilih</option>
+                                    @foreach ($kategori as $item)
+                                    <option value="{{ $item->nama_kategori }}">{{ $item->nama_kategori }}</option>
+                                    @endforeach
+                                </select>
+                            </x-group>
+                        </div>
+                    </div>
+                    
+                    
                 </div>
                 <div class="card-footer">
                     <x-btn-save />
