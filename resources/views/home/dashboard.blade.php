e12b5448e9e0c2b8714bd70350020d2a675ec191
ilhamdika <ilham27dika@gmail.com>
2024-05-19 20:17:17 +0700

    change dashboard admin

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.main',['title'=>'Dashboard'])
 @section('content')
 @can('admin')
-@include('home.admin')
+<!-- @include('home.admin') -->
+@include('home.manajer')
 @elsecan('manajer')
 @include('home.manajer')
 @else
